fa8f0c9668d1b5ad63c13b133c9f01c3ba3fcbc5
Anderson Neves <neves1697@gmail.com>
2024-10-10 19:27:36 -0300

    Ajustando a tela inicial. Ajustes de cores e gradientes

--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "../../Styles/TelaInicial.css"
 import Projetos from "../Projetos/Projetos";
+import FotoPerfil from "../../Assets/foto_perfil.jpg";
 
 const TelaInicial = () => {
     return <>
         <div className="container">
-            <div className="container-fundo">
-                <div className="container-fotoperfil">
-                    
-                    <div className="foto-perfil"></div>
+            <div className="container-efeito">
+                <div className="container-fundo">
+                    <div className="container-fotoperfil">
+                        <div className="foto-perfil">
+                            <img src={FotoPerfil} title="Foto de Perfil" alt="Foto de Perfil" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div className="teste"></div>
     </>
 }
 
