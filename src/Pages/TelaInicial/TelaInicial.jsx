c9f36a4fe67180e82f1c08e379edd82db62eefce
Anderson Neves <neves1697@gmail.com>
2024-10-11 14:04:14 -0300

    Ajustes na responsividade mobile da tela inicial

--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../../Styles/TelaInicial.css"
 import Projetos from "../Projetos/Projetos";
 import FotoPerfil from "../../Assets/foto_perfil.jpg";
+import "../../Styles/Mobile/TelaInicial.css";
 
 const TelaInicial = () => {
     return <>
@@ -11,9 +12,9 @@ const TelaInicial = () => {
                     <div className="foto-perfil">
                         <img src={FotoPerfil} title="Foto de Perfil" alt="Foto de Perfil" />
                     </div>
+                    <div className="teste"><p>t</p></div>
                 </div>
             </div>
-            
         </div>
     </>
 }
