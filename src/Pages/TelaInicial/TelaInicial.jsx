13b76f8f4f190b19d04730a7e4717f3630b856ea
Anderson Neves <neves1697@gmail.com>
2024-10-11 14:23:07 -0300

    Incluindo CARDS na tela inicial para descrever projetos

--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -5,14 +5,42 @@ import FotoPerfil from "../../Assets/foto_perfil.jpg";
 import "../../Styles/Mobile/TelaInicial.css";
 
 const TelaInicial = () => {
+
+    const cardData = [
+        {
+            title: "Projeto 1",
+            description: "Descrição do projeto 1",
+        },
+        {
+            title: "Projeto 2",
+            description: "Descrição do projeto 2",
+        },
+        {
+            title: "Projeto 3",
+            description: "Descrição do projeto 3",
+        },
+    ];
+
+
     return <>
         <div className="container">
             <div className="container-fundo">
                 <div className="container-fotoperfil">
                     <div className="foto-perfil">
                         <img src={FotoPerfil} title="Foto de Perfil" alt="Foto de Perfil" />
+
                     </div>
-                    <div className="teste"><p>t</p></div>
+
+
+                </div>
+                {/* Seção dos cards */}
+                <div className="card-container">
+                    {cardData.map((card, index) => (
+                        <div className="card" key={index}>
+                            <h3>{card.title}</h3>
+                            <p>{card.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
