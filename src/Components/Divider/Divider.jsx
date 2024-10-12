25e01ebd2883f0c3b8163588adb5222c465d8415
Anderson Neves <neves1697@gmail.com>
2024-10-12 17:15:54 -0300

    Adicionando um componente para o DIVIDER

---
+++ src/Components/Divider/Divider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../Styles/Divider.css";
+
+const Divider = () => {
+    return <>
+        <div className="container-divider">
+            <div className="divider">
+            </div>
+        </div>
+    </>
+}
+
+export default Divider;
--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -4,6 +4,7 @@ import "../../Styles/Mobile/TelaInicial.css";
 import FotoPerfil from "../../Assets/foto_perfil.jpg";
 import Cards from "../../Components/Cards/Cards";
 import Slides from "../../Components/Slide/Slide";
+import Divider from "../../Components/Divider/Divider";
 
 const TelaInicial = () => {
     return (
@@ -11,10 +12,7 @@ const TelaInicial = () => {
             <div className="container">
                 <img src={FotoPerfil} alt="Foto de Perfil" title="Foto de Perfil" className="foto-perfil" />
 
-                <div className="container-divider">
-                    <div className="divider">
-                    </div>
-                </div>
+                <Divider />
                 <Slides />
                 <Cards />
             </div>
