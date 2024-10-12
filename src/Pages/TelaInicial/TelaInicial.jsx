7ab1e3833979aab71bd0ee2efc8450cc912fc869
Anderson Neves <neves1697@gmail.com>
2024-10-12 16:31:22 -0300

    Refatorando a tela inicial, criei um componente para o (SLIDE) que fica passando de forma transversal na tela, mostrando as tecnologias

--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -2,54 +2,17 @@ import React from "react";
 import "../../Styles/TelaInicial.css";
 import FotoPerfil from "../../Assets/foto_perfil.jpg";
 import "../../Styles/Mobile/TelaInicial.css";
-import { IoLogoJavascript } from "react-icons/io5";
-import { BiLogoJava, BiLogoPostgresql } from "react-icons/bi";
-import { SiMysql, SiCsharp,SiPostman,SiOracle,SiExpo  } from "react-icons/si";
-import { FaCss3, FaHtml5,FaReact  } from "react-icons/fa";
-import { AiOutlinePython } from "react-icons/ai";
 import Cards from "../../Components/Cards/Cards";
+import Slides from "../../Components/Slide/Slide";
 
 const TelaInicial = () => {
-
-    const languages = [
-        { icon: <SiCsharp size={50} color="purple" />, alt: "C#" },
-        { icon: <BiLogoJava size={50} color="#F80000" />, alt: "Java" },
-        { icon: <SiMysql size={50} color="#00758F"/>, alt: "Mysql" },
-        { icon: <BiLogoPostgresql size={50} color="#336791" />, alt: "PostgreSQL" },
-        { icon: <FaHtml5 size={50} color="#E44D26" />, alt: "HTML" },
-        { icon: <FaCss3 size={50} color="#1572B6" />, alt: "CSS" },
-        { icon: <IoLogoJavascript size={50} color="yellow" />, alt: "JavaScript" },
-        { icon: <SiPostman  size={50} color="#FF6A24" />, alt: "Postman" },
-        { icon: <FaReact size={50} color="#61DAFB" />, alt: "React JS" },
-        { icon: <SiOracle size={50} color="#F80000" />, alt: "Oracle" },
-        { icon: <AiOutlinePython size={50} color="#306998" />, alt: "Python" },
-        { icon: <SiExpo size={50} color="white" />, alt: "Expo" },
-    ];
-
     return (
         <>
             <div className="container">
                 <div className="container-fundo">
                     <img src={FotoPerfil} alt="Foto de Perfil" title="Foto de Perfil" className="foto-perfil" />
                 </div>
-
-                <div className="language-container">
-                    <div className="language-slider">
-                        {/* Renderiza os ícones uma vez para a animação contínua */}
-                        {languages.map((language, index) => (
-                            <div key={index} className="language-card">
-                                {language.icon}
-                            </div>
-                        ))}
-                        {/* Duplicando os ícones apenas para a animação */}
-                        {languages.map((language, index) => (
-                            <div key={index + languages.length} className="language-card">
-
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
+                <Slides />
                 <Cards />
             </div>
         </>
