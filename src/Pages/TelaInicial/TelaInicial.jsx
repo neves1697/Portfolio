337bb9c2a55c01bb18868736abf49c05966636c4
Anderson Neves <neves1697@gmail.com>
2024-10-15 21:41:57 -0300

    Ajustando ícones na tela inicial

--- src/Pages/TelaInicial/TelaInicial.jsx
+++ src/Pages/TelaInicial/TelaInicial.jsx
@@ -5,13 +5,21 @@ import FotoPerfil from "../../Assets/foto_perfil.jpg";
 import Cards from "../../Components/Cards/Cards";
 import Slides from "../../Components/Slide/Slide";
 import Divider from "../../Components/Divider/Divider";
+import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 
 const TelaInicial = () => {
+
+    const linkedin = "https://www.linkedin.com/in/anderson-neves-405968118/";
+
     return (
         <>
             <div className="container">
                 <img src={FotoPerfil} alt="Foto de Perfil" title="Foto de Perfil" className="foto-perfil" />
-
+                <p>
+                    <a href={linkedin} target="_blank"><FaLinkedin size={40} color="#0077B5" title="Linkedin" /></a>
+                </p>
                 <Divider />
                 <Slides />
                 <Cards />
