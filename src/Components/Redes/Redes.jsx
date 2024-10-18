20d9b1461b1ba8887eee6889bc132dcc1aba8aad
Anderson Neves <neves1697@gmail.com>
2024-10-17 22:05:25 -0300

    Adicionando um componente chamado REDES para descrever as redes sociais que ficam abaixo da foto de perfil

---
+++ src/Components/Redes/Redes.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaLinkedin, FaGithub, FaGithubSquare } from "react-icons/fa";
+import "../../Styles/Redes.css";
+
+const Redes = () => {
+    const linkedin = "https://www.linkedin.com/in/anderson-neves-405968118/";
+    const github = "https://github.com/neves1697";
+    const redes = [
+        { nome: 'github', url: 'https://github.com/neves1697' },
+        { nome: 'linkedin', url: 'https://www.linkedin.com/in/anderson-neves-405968118/' }
+    ];
+
+    return <>
+        <div className="redes">
+            <a href={redes[1].url} target="_blank" title="Linkedin">
+                <FaLinkedin size={50} color="#0A66C2 " />
+            </a>
+
+            <a href={redes[0].url} title="Github">
+                <FaGithub size={50} color="#171515" />
+            </a>
+        </div>
+    </>
+}
+
+export default Redes;
