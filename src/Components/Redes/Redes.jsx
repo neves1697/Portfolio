a335ad1844ce85d8552be5cfe09133cac55a7365
Anderson Neves <neves1697@gmail.com>
2024-10-17 22:33:19 -0300

    Ajustando o componente de TELAINICIAL

--- src/Components/Redes/Redes.jsx
+++ src/Components/Redes/Redes.jsx
@@ -3,20 +3,18 @@ import { FaLinkedin, FaGithub, FaGithubSquare } from "react-icons/fa";
 import "../../Styles/Redes.css";
 
 const Redes = () => {
-    const linkedin = "https://www.linkedin.com/in/anderson-neves-405968118/";
-    const github = "https://github.com/neves1697";
     const redes = [
-        { nome: 'github', url: 'https://github.com/neves1697' },
-        { nome: 'linkedin', url: 'https://www.linkedin.com/in/anderson-neves-405968118/' }
+        { nome: 'Github', url: 'https://github.com/neves1697' },
+        { nome: 'Linkedin', url: 'https://www.linkedin.com/in/anderson-neves-405968118/' }
     ];
 
     return <>
         <div className="redes">
-            <a href={redes[1].url} target="_blank" title="Linkedin">
+            <a href={redes[1].url} target="_blank" title={redes[1].nome}>
                 <FaLinkedin size={50} color="#0A66C2 " />
             </a>
 
-            <a href={redes[0].url} title="Github">
+            <a href={redes[0].url} title={redes[0].nome}>
                 <FaGithub size={50} color="#c6c6c6" />
             </a>
         </div>
